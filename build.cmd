8622b85c3463a02075eb97b5a82e3c5dca7be408
Rc-Cookie <rccookie@mail.de>
2023-10-24 13:47:46 +0200

    Added simple build script

---
+++ build.cmd
@@ -0,0 +1,1 @@
+java -jar .\chrome-porter\out\artifacts\web_ext_chrome_porter_jar\chrome-porter.jar . && web-ext build -s Firefox -a .\Firefox\web-ext-artifacts\ %* && web-ext build -s Chrome -a .\Chrome\web-ext-artifacts\ %*
